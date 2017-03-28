4cde8a8f61470eb0adbfeae092c372b0b43fd1cc
ljy <386443153@qq,com>
2017-03-28 06:06:45 +0000

    curl.php

--- curl.php
+++ curl.php
@@ -1,1 +1,19 @@
-# APPDEV
+<?php
+$value =  $_POST ['value'];   //take the value of "value" field
+$from = $_POST['from'];       //take the value of "from" field
+$ip = $_SERVER['REMOTE_ADDR']; //the client's IP address
+echo "Your value = ".$value." from=".$from.".And your IP address is ".$ip."\n";
+$today=date("Y-m-d H:i:s");  //current time
+$file = "curltest.log";   //the log file name
+if(file_exists($file)){
+	$fp = fopen($file, "a");   //open the file in appending mode
+}
+else{
+	$fp = fopen($file, "w");   //open the file in writing ,mode
+}
+//prepare a line of record to write to the file, in CSV format
+$record = $today . "," . $from . "," . $ip . "," . $value . "\n";
+fwrite($fp, $record);         //write to file
+fclose($fp);   //close the open file
+
+?>
